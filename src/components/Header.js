f79d2a454ea1c4c32ccc7f44207241ef9034fe3f
Abhay Singh <abhay.singh@gmail.com>
2025-05-10 16:05:41 -0500

    V2

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Header() {
   return (
     <div className="header">
-      <h1>Word Context Game</h1>
+      <h1>Word Context Game - By Olivia Singh</h1>
       <p>Find the right meaning based on the context!</p>
     </div>
   );
